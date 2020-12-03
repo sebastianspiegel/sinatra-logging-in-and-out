c29c5fda054fa9db67fde2c50fd87e08f92879d9
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2020-12-03 17:54:24 -0500

    commit tree aa2c4d98df4a736c44b0e072c40304f6aa27713f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,26 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by("username" => params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
+    erb :error 
+  end 
 
-  end
 
   get '/account' do
-
+    erb :account 
   end
 
   get '/logout' do
+    session.clear
+    redirect '/'
+  end
 
+  get '/error' do
+    erb :error 
   end
 
 
